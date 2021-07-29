4be7dd2c88349948133fd0c28a634887f337d9ad
aseel-farrar <aseelfarrar1@hotmail.com>
2021-07-29 18:27:23 +0300

    init setup

---
+++ src/main/java/com/example/bayMax/BayMaxApplication.java
@@ -0,0 +1,13 @@
+package com.example.bayMax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BayMaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bayMax/BayMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bayMax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BayMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
