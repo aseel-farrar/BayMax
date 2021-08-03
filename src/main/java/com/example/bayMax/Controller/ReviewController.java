bf8f40539798a225790bea73ec516fda12d74800
muhmadjaradat <muhmadjradat@gmail.com>
2021-08-03 15:12:26 +0300

    $

---
+++ src/main/java/com/example/bayMax/Controller/ReviewController.java
@@ -0,0 +1,66 @@
+package com.example.bayMax.Controller;
+
+import com.example.bayMax.Domain.Requests;
+import com.example.bayMax.Domain.Reviews;
+import com.example.bayMax.Domain.Users;
+import com.example.bayMax.Infrastructure.ReviewsRepository;
+import com.example.bayMax.Infrastructure.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ReviewController {
+
+    @Autowired
+    ReviewsRepository reviewsRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    // get all reviews
+    @GetMapping("/reviews")
+    public String getReviews(Model model){
+        List<Reviews> reviews = reviewsRepository.findAll();
+        model.addAttribute("reviews", reviews);
+        return "reviews";
+    }
+
+    // add new review
+    @PostMapping("/addReviews")
+    public RedirectView addReview(@RequestParam Long id, @RequestParam String body, Principal principal){
+        Users doctor = userRepository.findById(id).orElseThrow();
+        Users patient = userRepository.findUsersByUsername(principal.getName());
+        Reviews review = new Reviews(body);
+        review.setDoctor(doctor.getFullName());
+        review.setUser(patient);
+
+//        userRepository.save(doctor);
+        reviewsRepository.save(review);
+        return new RedirectView("/reviews");
+    }
+
+    // get add review form
+    @GetMapping("/addReviews")
+    public String getAddForm(Model model, Principal principal){
+        Users newUser = userRepository.findUsersByUsername(principal.getName());
+        List<Users> doctors = new ArrayList<>();
+        for (Requests request: newUser.getPatientRequests()
+        ) {
+            if (request.isAccepted()){
+                doctors.add(request.getDoctor());
+            }
+        }
+        model.addAttribute("doctors",doctors);
+        return "reviewForm";
+    }
+
+}
---
+++ src/main/java/com/example/bayMax/Infrastructure/ReviewsRepository.java
@@ -0,0 +1,9 @@
+package com.example.bayMax.Infrastructure;
+
+import com.example.bayMax.Domain.Reviews;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewsRepository extends JpaRepository<Reviews,Long> {
+}
